1083c244e08204e34086287b3be3cc8c388f074c
BegLand <69076818+BegLand@users.noreply.github.com>
2020-08-07 04:26:01 +0200

    Create default_code.js

---
+++ default_code.js
@@ -0,0 +1,37 @@
+//even with 400ms, can attack smooth
+
+var attack_mode=true
+setInterval(function(){
+
+  heal();
+	loot();
+
+	if(!attack_mode || character.rip || is_moving(character)) return;
+
+	var target=get_targeted_monster();
+	if(!target)
+	{
+		target=get_nearest_monster({min_xp:100,max_att:120});
+		if(target) change_target(target);
+		else
+		{
+			set_message("No Monsters");
+			return;
+		}
+	}
+	
+	if(!is_in_range(target))
+	{
+		move(
+			character.x+(target.x-character.x)/2,
+			character.y+(target.y-character.y)/2
+			);
+		// Walk half the distance
+	}
+	else if(is_in_range(target))
+	{
+		set_message("Attacking");
+		attack(target);
+	}
+
+},400); // Loops every 1/4 seconds.
